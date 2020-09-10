01f346eea96fca02cc664cfa7a6762b85c646db8
Uddeshya Tyagi <54842397+uddeshyatyagi@users.noreply.github.com>
2020-09-10 20:38:48 +0530

    Update passwd_gen.sh

--- passwd_gen.sh
+++ passwd_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#This wil generate random password
+#This will generate random password
 
-echo "Enter the length of the password to be genrated"
+echo "Enter the length of the password to be generated"
 read pass_length
 
 for i in $(seq 1 5):
@@ -9,6 +9,6 @@ do
 	openssl rand -base64 48 | cut -c1-$pass_length
 done
 
-: "we could have used hex in place of base 64 but it will only give  acombination of small letters and numbers.
+: "we could have used hex in place of base 64 but it will only give a combination of small letters and numbers.
 
 "
